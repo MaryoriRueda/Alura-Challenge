7d83b7a9fb2efe87d86119b2a80dc7d0d564b30c
Tatiana Rueda <maryori.tatiana@gmail.com>
2023-05-30 18:27:35 -0500

    Primer commit

---
+++ script.js
@@ -0,0 +1,51 @@
+const textArea=document.querySelector(".text-ingrese");
+const mensaje=document.querySelector(".mensaje");
+const copiar=document.querySelector(".copiar")
+/*La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"*/
+function btnEncriptar(){
+    const textoencriptado = encriptar(textArea.value)
+    mensaje.value = textoencriptado
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"
+}
+
+function encriptar(stringEncriptado){
+    let matrizCodigo = [["e", "enter"],["i", "imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringEncriptado = stringEncriptado.toLowerCase()
+
+    for(let i = 0; i<matrizCodigo.length;i++){
+        if(stringEncriptado.includes(matrizCodigo[i][0])){
+            stringEncriptado=stringEncriptado.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptado
+}
+
+function btnDesencriptar(){
+    const textoencriptado = desencriptar(textArea.value)
+    mensaje.value = textoencriptado
+    textArea.value = "";
+    
+}
+
+function desencriptar(stringDesencriptado){
+    let matrizCodigo = [["e", "enter"],["i", "imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringDesencriptado = stringDesencriptado.toLowerCase()
+
+    for(let i = 0; i<matrizCodigo.length;i++){
+        if(stringDesencriptado.includes(matrizCodigo[i][1])){
+            stringDesencriptado=stringDesencriptado.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0])
+        }
+    }
+    return stringDesencriptado
+}
+
+
+copiar.addEventListener("click",()=>{
+    let text=mensaje;
+    navigator.clipboard.writeText(text.value);
+})
